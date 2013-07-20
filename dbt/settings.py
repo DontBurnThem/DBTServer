87913e80de4d9caa884e708af167b6fbd09ce038
ermanno <ermanno.moser@yahoo.it>
2013-07-20 16:08:11 +0200

    rinominato

--- dbt/settings.py
+++ dbt/settings.py
@@ -115,7 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'dbtapp'
+    'core',
 )
 
 # A sample logging configuration. The only tangible logging
--- dbtapp/admin.py
+++ dbtapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from dbtapp.models import Book,Offer,User
+from core.models import Book,Offer,User
 
 admin.site.register(Book)
 admin.site.register(Offer)
