7f3c8adf8e05b6be66c7e5674a95cc7d75afb1f6
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-07-21 05:51:13 +0200

    Add LocalOfferManager to get local offers

--- dbtcore/models.py
+++ dbtcore/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from math import asin
+
+class LocalOfferManager(models.Manager):
+    def get_local(self, point, distance):
+        """This method returns a queryset returning only local results
+
+        :point: a 2-tuple of (latitude, longitude) of the search center
+        :distance: The search distance in kilometers.
+        :returns: a pre-filtered queryset with local results only
+
+        """
+        d_alpha = asin(distance/20037.0) #Get angle from distance 20037 is the
+        # Earth radius. The above formula approximates the arc with a triangle
+        # side. Could be better, could be worse. It works quite fine for small
+        # distances.
+        return super(LocalOfferManager, self).get_query_set(
+                ).filter(lat__range=(point[0]-d_alpha,point[0]+d_alpha)
+                ).filter(lon__range=(point[1]-d_alpha,point[1]+d_alpha))
 
 class Book(models.Model):
     isbn = models.IntegerField(primary_key = True)
@@ -26,6 +44,9 @@ class Offer(models.Model):
     lon = models.FloatField()
     user = models.ForeignKey(User)
     book = models.ForeignKey(Book)
+
+    objects = models.Manager()
+    local_objects = LocalOfferManager()
 
     def __unicode__(self):
         return "Book ISBN: " + str(self.book.isbn) + " for " + str(self.price) + " credits."
